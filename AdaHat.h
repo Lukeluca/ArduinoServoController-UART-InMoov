bb85a8f60eed680c054bba09cf1f25864ae395d4
Luke Wallace <Lukeluca@users.noreply.github.com>
2021-10-08 06:11:19 -0500

    Updating for multiple Servo hats

--- AdaHat.h
+++ AdaHat.h
@@ -10,16 +10,20 @@
 class AdaHat {
 
   private:
-    Adafruit_PWMServoDriver _pwm;
+    Adafruit_PWMServoDriver _pwm0;
+    Adafruit_PWMServoDriver _pwm1;
 
     // safe defaults if something else goes wrong, aka 90 degrees
-    int _servoMins[16] = { 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375 };
-    int _servoMaxs[16] = { 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375 };
+    int _servoMins[32] = { 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375,
+                           375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375 };
+    int _servoMaxs[32] = { 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375,
+                           375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375, 375 };
 
     // last sent servo positions (in degrees)
-    int _servoPoss[16] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
-    unsigned long _lastUpdated[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    bool _isAttached[16] = { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+    int _servoPoss[32] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                           -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+    unsigned long _lastUpdated[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
   public:
     AdaHat(int boardPosition);
@@ -31,4 +35,3 @@ class AdaHat {
 };
 
 #endif
-
